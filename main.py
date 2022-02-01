bdc253291344c661282624638d5a22e85d8fb08d
mikha-el82 <mikhael@centrum.cz>
2022-02-01 10:16:54 +0100

    Updates

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import datetime
 
 file_name = input("Name of the file to convert (excl. '.csv'): ") + ".csv"
 formatted_file_name = input("Name of the formatted file (excl. '.csv'): ") + ".csv"
+dish_weight_str = input("Empty Petri dish weight [g]: ")
+dish_weight = float(dish_weight_str)
 excel_file_name = formatted_file_name.replace(".csv", ".xlsx")
-first_row = "Datum;Hmotnost [g]; Uplynulý čas [hh:hh:ss];Uplynulý čas [h]; m/m0\n"
+dish_weight_row = f"Hmotnost prázdné Petriho misky [g]:;{dish_weight_str.replace('.', ',')}\n"
+header_row = "Datum;Hmotnost [g];Hmotnost malty [g];Uplynulý čas [hh:hh:ss];Uplynulý čas [h]; m/m0\n"
 
 with open(file_name) as datafile:
     data_lines = datafile.readlines()
@@ -12,22 +15,31 @@ with open(file_name) as datafile:
 split_data_lines = [row.strip().rsplit(";") for row in data_lines]
 
 with open(formatted_file_name, "a") as formatted_datafile:
-    formatted_datafile.write(first_row)
+    formatted_datafile.write(dish_weight_row)
+    formatted_datafile.write(header_row)
     number_of_rows = 0
     for row in split_data_lines:
         number_of_rows += 1
         if "initial_grams" not in globals():
-            initial_grams = float(row[0])
-            print(f"Initial weight: {initial_grams}")
+            initial_grams = float(row[0])-dish_weight
+            print(f"Initial weight of mortar: {initial_grams}")
         grams = float(row[0])
         grams_comma = str(grams).replace(".", ",")
-        ratio = round(grams / initial_grams, 4)
+        grams_mortar = grams - dish_weight
+        grams_mortar_comma = str(grams_mortar).replace(".", ",")
+        ratio = round(grams_mortar / initial_grams, 4)
         ratio_comma = str(ratio).replace(".", ",")
 
-        date = row[2].rsplit("/")
-        month = int(date[0])
-        day = int(date[1])
-        year = int(date[2])
+        if "/" in row[2]:
+            date = row[2].rsplit("/")
+            month = int(date[0])
+            day = int(date[1])
+            year = int(date[2])
+        if "." in row[2]:
+            date = row[2].rsplit(".")
+            month = int(date[0])
+            day = int(date[1])
+            year = int(date[2])
 
         formatted_date = f"{day}.{month}.{year}"
 
@@ -43,7 +55,15 @@ with open(formatted_file_name, "a") as formatted_datafile:
             if hours == 12:
                 hours = 0
             seconds = int(raw_seconds.replace(" dop.", ""))
-        formatted_time = f"{hours}:{minutes}:{seconds}"
+        if minutes < 10:
+            minutes_str = f"0{minutes}"
+        else:
+            minutes_str = str(minutes)
+        if seconds < 10:
+            seconds_str = f"0{seconds}"
+        else:
+            seconds_str = str(seconds)
+        formatted_time = f"{hours}:{minutes_str}:{seconds_str}"
 
         if "initial_datetime" not in globals():
             initial_datetime = datetime.datetime(year, month, day, hours, minutes, seconds)
@@ -59,7 +79,7 @@ with open(formatted_file_name, "a") as formatted_datafile:
         elapsed_time_hours_only = round(elapsed_hours + elapsed_minutes / 60 + elapsed_seconds / 3600, 4)
         elapsed_time_hours_only_comma = str(elapsed_time_hours_only).replace(".", ",")
 
-        formatted_row = f"{formatted_date} {formatted_time};{grams_comma};{elapsed_formatted};" \
+        formatted_row = f"{formatted_date} {formatted_time};{grams_comma};{grams_mortar_comma};{elapsed_formatted};" \
                         f"{elapsed_time_hours_only_comma};{ratio_comma}\n "
         formatted_datafile.write(formatted_row)
 
